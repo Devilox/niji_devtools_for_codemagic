f9f058efcd3adf58c90f475e0a291fd9796e264e
François Lebeux <francois.lebeux@niji.fr>
2021-11-19 14:50:05 +0100

    first commit

---
+++ push.sh
@@ -0,0 +1,7 @@
+echo "# niji_devtools_for_codemagic" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Devilox/niji_devtools_for_codemagic.git
+git push -u origin main
